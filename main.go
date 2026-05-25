3b8cc038805b2c64ab7266eba2a79a844cf05397
Santawat Santiteerakul <santawat.s@gmail.com>
2026-05-25 16:58:38 +0700

    3-structured-log: output formats JSON

--- main.go
+++ main.go
@@ -102,7 +102,7 @@ func initializeLogger(logFilename string) (*slog.Logger, closeFunc, error) {
 			}
 			return nil
 		}
-		handlers = append(handlers, slog.NewTextHandler(buffer, &slog.HandlerOptions{
+		handlers = append(handlers, slog.NewJSONHandler(buffer, &slog.HandlerOptions{
 			Level: slog.LevelInfo,
 		}))
 		closers = append(closers, close)
