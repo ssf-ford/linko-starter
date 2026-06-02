b14a218c98736de1a30f6c268a801b90236304e4
Santawat Santiteerakul <santawat.s@gmail.com>
2026-06-02 13:12:19 +0700

    9-metrics: service metrics

--- server.go
+++ server.go
@@ -13,6 +13,8 @@ import (
 	"time"
 
 	"boot.dev/linko/internal/store"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 type server struct {
@@ -145,6 +147,7 @@ func newServer(store store.Store, port int, cancel context.CancelFunc, logger *s
 	mux.Handle("POST /api/shorten", s.authMiddleware(http.HandlerFunc(s.handlerShortenLink)))
 	mux.Handle("GET /api/stats", s.authMiddleware(http.HandlerFunc(s.handlerStats)))
 	mux.Handle("GET /api/urls", s.authMiddleware(http.HandlerFunc(s.handlerListURLs)))
+	mux.Handle("GET /metrics", promhttp.Handler())
 	mux.HandleFunc("GET /{shortCode}", s.handlerRedirect)
 	mux.HandleFunc("POST /admin/shutdown", s.handlerShutdown)
 
